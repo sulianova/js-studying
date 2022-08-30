843b565349483f10f874c3bb4b3b4ecc0fea85e5
sulianova <ulianova.sofia@gmail.com>
2022-08-30 13:46:34 +0300

    Added app2

---
+++ problems-by-topic/dom-api/app2.js
@@ -0,0 +1,32 @@
+// ПЕРЕКЛЮЧАЕМОЕ МЕНЮ
+
+export default () => {
+    const handle = (event, container) => {
+        const targetTab = event.target;
+        if (targetTab.classList.contains('active')) {
+          return;
+        }
+    
+        const targetTabContentId = targetTab.dataset.bsTarget;
+        const targetTabContent = document.querySelector(targetTabContentId);
+    
+        const activeTab = container.querySelector('.active');
+        const activeTabContentId = activeTab.dataset.bsTarget;
+        const activeTabContent = document.querySelector(activeTabContentId);
+    
+        targetTab.classList.add('active');
+        targetTabContent.classList.add('active');
+    
+        activeTab.classList.remove('active');
+        activeTabContent.classList.remove('active');
+      };
+
+    const navs = document.querySelectorAll('.nav');
+    navs.forEach((nav) => {
+            const tabs = nav.querySelectorAll('[data-bs-toggle]');
+            tabs.forEach((tab) => {
+              tab.addEventListener('click', (event) => handle(event, nav));
+            });
+          });
+ };
+

b235ac9f09b12c5bf555719773a1e485cfe22d07
sulianova <ulianova.sofia@gmail.com>
2022-08-31 17:50:44 +0300

    Added watch

---
+++ problems-by-topic/async/watch.js
@@ -0,0 +1,25 @@
+// ФУНКЦИЯ, КОТОРАЯ СЛЕДИТ ЗА ИЗМЕНЕНИЕМ ФАЙЛА С ЗАДАННОЙ ПЕРИОДИЧНОСТЬЮ
+
+import fs from 'fs';
+
+export default (filepath, period, cb) => {
+    let lastCheckTime = Date.now();
+  
+    const check = (timerId) => {
+      fs.stat(filepath, (err, stats) => {
+        if (err) {
+          clearInterval(timerId);
+          cb(err);
+          return;
+        }
+        const { mtimeMs } = stats;
+        if (mtimeMs > lastCheckTime) {
+          cb(null);
+          lastCheckTime = mtimeMs;
+        }
+      });
+    };
+  
+    const timerId = setInterval(() => check(timerId), period);
+    return timerId;
+  };

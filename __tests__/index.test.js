87c6116895ee186c8491dcff8cbdf772771ebf5d
sulianova <ulianova.sofia@gmail.com>
2022-08-31 14:13:08 +0300

    Added app3

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,8 +1,54 @@
 //NODE_OPTIONS=--experimental-vm-modules npx jest
 
-import reverse from '../src/index.js';
 
-test('reverse', () => {
-  expect(reverse('hello')).toEqual('olleh');
-  expect(reverse('')).toEqual('');
+import '@testing-library/jest-dom';
+import fs from 'fs';
+import path from 'path';
+import { htmlEscape } from 'escape-goat';
+import testingLibraryDom from '@testing-library/dom';
+import userEvent from '@testing-library/user-event';
+
+import run from '../src/application.js';
+
+const { screen } = testingLibraryDom;
+
+beforeEach(() => {
+  const initHtml = fs.readFileSync(path.join('__fixtures__', 'index.html')).toString();
+  document.body.innerHTML = initHtml;
+  run();
+});
+
+test('application 1', async () => {
+  const sendButton = screen.getByRole('button', { name: 'Send' });
+  const emailField = screen.getByRole('textbox', { name: 'Email' });
+  const nameField = screen.getByRole('textbox', { name: 'Name' });
+  await userEvent.type(emailField, 'a@b.c');
+  await userEvent.type(nameField, 'Toto');
+  await userEvent.click(sendButton);
+
+  const sendText = screen.getByText('Feedback has been sent');
+  const emailText = screen.getByText('Email: a@b.c');
+  const nameText = screen.getByText('Name: Toto');
+
+  expect(sendText).toBeInTheDocument();
+  expect(emailText).toBeInTheDocument();
+  expect(nameText.outerHTML).toEqual('<div>Name: Toto</div>');
+});
+
+test('application 2', async () => {
+  const sendButton = screen.getByRole('button', { name: 'Send' });
+  const emailField = screen.getByRole('textbox', { name: 'Email' });
+  const nameField = screen.getByRole('textbox', { name: 'Name' });
+  const commentField = screen.getByRole('textbox', { name: 'Comment' });
+  await userEvent.type(emailField, 'toto@hexlet.io');
+  await userEvent.type(nameField, 'Toto Robbins');
+  await userEvent.type(commentField, 'If you <i>can</i> do, you <b>must</b> do');
+  await userEvent.click(sendButton);
+
+  const sendText = screen.getByText('Feedback has been sent');
+  const commentText = screen.getByText(/Comment.*/);
+
+  expect(sendText).toBeInTheDocument();
+  expect(commentText).toBeInTheDocument();
+  expect(commentText.outerHTML).toEqual(`<div>Comment: ${htmlEscape('If you <i>can</i> do, you <b>must</b> do')}</div>`);
 });
---
+++ problems-by-topic/dom-api/app3.js
@@ -0,0 +1,27 @@
+//ПОЛУЧИТЬ ДАННЫЕ ИЗ ФОРМЫ И ЭКРАНИРОВАТЬ ИХ
+
+import { htmlEscape } from 'escape-goat';
+
+const render = (element, data) => {
+    const div = document.createElement('div');
+    const { email, name, comment } = data;
+    div.innerHTML = `
+      <p>Feedback has been sent</p>
+      <div>Email: ${htmlEscape(email)}</div>
+      <div>Name: ${htmlEscape(name)}</div>
+      <div>Comment: ${htmlEscape(comment)}</div>
+    `;
+    element.replaceWith(div);
+};
+
+export default () => {
+    const formElement = document.querySelector('.feedback-form');
+    const handle = (element) => {
+      element.preventDefault();
+      const formData = new FormData(element.target);
+  
+      render(formElement, Object.fromEntries(formData));
+    };
+
+    formElement.addEventListener('submit', handle);
+};

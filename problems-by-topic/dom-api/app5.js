f77bbc909f9eabc079fa18c0e40f1d274f74d489
sulianova <ulianova.sofia@gmail.com>
2022-09-01 16:01:13 +0300

    Added app5

---
+++ problems-by-topic/dom-api/app5.js
@@ -0,0 +1,33 @@
+// АВТОКОМПЛИТ
+
+import 'whatwg-fetch';
+import { htmlEscape } from 'escape-goat';
+
+export default () => {
+  const autocompleteElements = document.querySelectorAll(
+    'input[data-autocomplete]',
+  );
+  autocompleteElements.forEach((element) => {
+    const { origin } = window.location;
+    const pathname = element.dataset.autocomplete;
+    const dataAutocompleteName = element.dataset.autocompleteName;
+
+    element.addEventListener('input', async (event) => {
+      const subString = event.target.value;
+      const parsedUrl = new URL(pathname, origin);
+      parsedUrl.searchParams.append('search', subString);
+
+      const response = await fetch(parsedUrl);
+      const items = await response.json();
+
+      const list = document.querySelector(
+        `ul[data-autocomplete-name="${dataAutocompleteName}"]`,
+      );
+      const options = items.length === 0 ? ['Nothing'] : items;
+      const listHTML = options
+        .map((item) => `<li>${htmlEscape(item)}</li>`)
+        .join('\n');
+      list.innerHTML = listHTML;
+    });
+  });
+};

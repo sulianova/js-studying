ec87d599137074837063e1e1571b460d19cf6e39
sulianova <ulianova.sofia@gmail.com>
2022-09-03 10:07:48 +0300

    Added progressBar

---
+++ problems-by-topic/dom-api/progressBar.js
@@ -0,0 +1,11 @@
+// Загрузка на 1% в секунду
+export default () => {
+    const element = document.querySelector('progress');
+    const handler = (counter = 0) => {
+      if (counter <= 100) {
+        element.setAttribute('value', counter);
+        setTimeout(() => handler(counter + 1), 1000);
+      }
+    };
+    handler();
+  };

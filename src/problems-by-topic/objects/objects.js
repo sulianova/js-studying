8292685a43bffea5192c3d407051d3cb0ff97a02
sulianova <ulianova.sofia@gmail.com>
2022-09-12 17:22:55 +0300

    Add genDiff

--- src/problems-by-topic/objects/objects.js
+++ src/problems-by-topic/objects/objects.js
@@ -128,3 +128,26 @@ export const bqs = (data) => {
 
   return result.join('&');
 };
+
+// import _ from 'lodash';
+export const genDiff = (data1, data2) => {
+  const keys1 = Object.keys(data1);
+  const keys2 = Object.keys(data2);
+  const keys = _.union(keys1, keys2);
+
+  const result = {};
+  for (const key of keys) {
+    if (!Object.hasOwn(data1, key)) {
+      result[key] = 'added';
+    } else if (!Object.hasOwn(data2, key)) {
+      result[key] = 'deleted';
+    } else if (data1[key] !== data2[key]) {
+      result[key] = 'changed';
+    } else {
+      result[key] = 'unchanged';
+    }
+  }
+
+  return result;
+};
+

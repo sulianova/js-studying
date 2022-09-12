7a7537269bfe8369d837eed204160ecf6991867f
sulianova <ulianova.sofia@gmail.com>
2022-09-12 14:09:13 +0300

    Separate functions and descriptions in objects

--- src/problems-by-topic/objects/objects.js
+++ src/problems-by-topic/objects/objects.js
@@ -1,32 +1,4 @@
-// Добавить в объект новый ключ со значением
-const staff = {
-  'general director': {
-    name: 'Belkin Igor',
-    age: '45',
-    married: true,
-    children: false,
-  },
-
-  'product manager': {
-    name: 'Eliseeva Helen',
-    age: '35',
-    married: false,
-    children: ['Ivan', 'Alex'],
-  },
-};
-
-staff['general director'].number = '88009870478';
-staff['product manager'].number = '88009870000';
-
-/*  Реализуйте и экспортируйте по умолчанию функцию,
-      которая "нормализует" данные переданного урока.
-      То есть приводит их к определенному виду. Нормализация происходит
-      путём изменения исходного объекта.
-      Функция должна обновлять содержимое урока по следующим правилам:
-        Имя капитализируется.
-        Весь текст описания приводится к нижнему регистру.
-  */
-// import _ from "lodash";
+import _ from "lodash";
 
 export const normalize = (data) => {
   data.name = _.capitalize(data.name);
@@ -34,21 +6,6 @@ export const normalize = (data) => {
   return data;
 };
 
-const lesson = {
-  name: 'ДеструКТУРИЗАЦИЯ',
-  description: 'каК удивитЬ друзей',
-};
-
-normalize(lesson);
-// => { name: 'Деструктуризация', description: 'как удивить друзей' }
-
-/*  Реализуйте и экспортируйте по умолчанию функцию,
-      которая сравнивает объекты по совпадению данных,
-      а не по ссылкам. Эта функция принимает на вход
-      две компании и возвращает true, если их структура одинаковая,
-      и false в обратном случае. Проверка должна проходить
-      по свойствам name, state, website.
-  */
 export const is = (company1, company2) => {
   const keys = ['name', 'state', 'website'];
   for (const key of keys) {
@@ -60,26 +17,7 @@ export const is = (company1, company2) => {
   return true;
 };
 
-const company1 = {
-  name: 'Hexlet',
-  state: 'moderating',
-  website: 'https://hexlet.io',
-};
-const company2 = {
-  name: 'CodeBasics',
-  state: 'published',
-  website: 'https://code-basics.com',
-};
-
-is(company1, company2);
-// => false
-
-/* Реализуйте и экспортируйте по умолчанию функцию,
-  которая принимает на вход имя сайта и возвращает информацию о нем.
-  Если домен передан без указания протокола, то по умолчанию берется http
-  */
 // import _ from 'lodash';
-
 export const getDomainInfo = (domain) => {
   let scheme = '';
   if (domain.startsWith('https://')) {
@@ -94,20 +32,7 @@ export const getDomainInfo = (domain) => {
   return { scheme, name };
 };
 
-getDomainInfo('https://hexlet.io');
-// => { scheme: 'https', name: 'hexlet.io' }
-
-/*  Реализуйте и экспортируйте по умолчанию функцию,
-      которая считает количество слов в предложении,
-      и возвращает объект. В объекте свойства — это слова
-      (приведенные к нижнему регистру), а значения — это то,
-      сколько раз слово встретилось в предложении.
-      Слова в предложении могут находиться в разных регистрах.
-      Перед подсчетом их нужно приводить в нижний регистр,
-      чтобы не пропускались дубли.
-  */
 // import _ from 'lodash';
-
 export const countWords = (str) => {
   const strLowerCase = str.toLowerCase();
   const words = _.words(strLowerCase);
@@ -124,16 +49,6 @@ export const countWords = (str) => {
   return result;
 };
 
-const text1 = 'another one sentence with strange Words words';
-countWords(text1);
-// => { another: 1, one: 1, sentence: 1, with: 1, strange: 1, words: 2 }
-
-/*  Реализуйте и экспортируйте функцию,
-      которая формирует из переданного объекта другой объект,
-      включающий только указанные свойства. Параметры:
-        Исходный объект
-        Массив имен свойств
-  */
 export const pick = (data, keys) => {
   const result = {};
 
@@ -146,21 +61,6 @@ export const pick = (data, keys) => {
   return result;
 };
 
-const data2 = {
-  user: 'ubuntu',
-  cores: 4,
-  os: 'linux',
-};
-
-pick(data2, ['user']); // { user: 'ubuntu' }
-
-/*  Реализуйте и экспортируйте функцию,
-      которая извлекает из объекта любой глубины
-      вложенности значение по указанным ключам. Параметры:
-        исходный объект
-        цепочка ключей (массив), по которой ведётся поиск значения
-      В случае, когда добраться до значения невозможно, возвращается null.
-  */
 export const get = (data, keys) => {
   let current = data;
   for (const key of keys) {
@@ -173,84 +73,13 @@ export const get = (data, keys) => {
   return current;
 };
 
-const data3 = {
-  user: 'ubuntu',
-  hosts: {
-    0: {
-      name: 'web1',
-    },
-    1: {
-      name: 'web2',
-      null: 3,
-      active: false,
-    },
-  },
-};
-
-get(data3, ['user', 'ubuntu']); // null
-get(data3, ['hosts', 1, 'name']); // 'web2'
-get(data3, ['hosts', 0]); // { name: 'web1' }
-
-// Альтернатива get в библиотеке lodash
 // import _ from 'lodash';
-
-// const obj = {};
-// const value = _.get(obj, 'one.two.three', 'defaultValue'); // 'defaultValue'
-
-// Альтернатива с try/catch
-// const get = (data, keys) => {
-//   let subData = data;
-
-//   for (let i = 0; i < keys.length; i++) {
-//     console.log(`Current subData is ${JSON.stringify(subData)}`);
-//     console.log(`I = ${i}, key = ${keys[i]}`);
-//     subData = getSubObject(subData, keys[i]);
-//   }
-
-//   return (subData == undefined)? null: subData;
-
-//   function getSubObject(subData, key) {
-//     try {
-//       return subData[key];
-//     } catch {
-//       return null;
-//     }
-//   }
-// };
-
-/*  Реализуйте и экспортируйте по умолчанию функцию,
-      которая заполняет объект данными из другого объекта
-      по разрешенному списку ключей. Параметры:
-        Исходный объект
-        Список ключей которые нужно заменить
-        Данные, которые нужно сливать в исходный объект
-    В случае, когда список ключей пустой, нужно сливать все данные полностью.
-  */
-// import _ from 'lodash';
-
 export const fill = (current, keys, data) => {
   const filteredData = keys.length > 0 ? _.pick(data, keys) : data;
   Object.assign(current, filteredData);
 };
 
-const company3 = {
-  name: null,
-  state: 'moderating',
-};
 
-const data = {
-  name: 'Hexlet',
-  state: 'published',
-};
-
-fill(company3, ['name'], data);
-// {
-//   name: 'Hexlet',
-//   state: 'moderating',
-// }
-
-// Реализуйте и экспортируйте функцию,
-// которая выполняет глубокое копирование объектов.
 export const cloneDeep = (object) => {
   const result = {};
   const entries = Object.entries(object);
@@ -261,17 +90,6 @@ export const cloneDeep = (object) => {
   return result;
 };
 
-/*  Реализуйте и экспортируйте по умолчанию функцию,
-      которая создает объект компании и возвращает его.
-      Для создания компании обязательно только одно
-      свойство – имя компании. Остальные свойства
-      добавляются только если они есть. Параметры:
-        Имя
-        Объект с дополнительными свойствами
-      Также у компаний есть два свойства со значениями по умолчанию:
-        state – moderating
-        createdAt – текущая дата
-  */
 export const make = (name, data = {}) => {
   const defaultData = {
     state: 'moderating',
@@ -281,19 +99,6 @@ export const make = (name, data = {}) => {
   return { ...defaultData, ...data, name };
 };
 
-make('Hexlet', { website: 'hexlet.io', state: 'published' });
-// {
-//   name: 'Hexlet',
-//   website: 'hexlet.io',
-//   state: 'published',
-//   createdAt: <тут текущая дата>
-// }
-
-/*  Реализуйте и экспортируйте по умолчанию функцию,
-      которая принимает на вход список пользователей,
-      извлекает их имена, сортирует в алфавитном порядке
-      и возвращает отсортированный список имен.
-  */
 export const getSortedNames = (users) => {
   const names = [];
 
@@ -303,12 +108,3 @@ export const getSortedNames = (users) => {
 
   return names.sort();
 };
-
-const users = [
-  { name: 'Bronn', gender: 'male', birthday: '1973-03-23' },
-  { name: 'Reigar', gender: 'male', birthday: '1973-11-03' },
-  { name: 'Eiegon', gender: 'male', birthday: '1963-11-03' },
-  { name: 'Sansa', gender: 'female', birthday: '2012-11-03' },
-];
-
-getSortedNames(users); // ['Bronn', 'Eiegon', 'Reigar', 'Sansa']

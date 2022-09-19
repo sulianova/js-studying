f02baff9f82f388e2223444cdfc9d673cad1a928
sulianova <ulianova.sofia@gmail.com>
2022-09-19 19:37:05 +0300

    Add merge in functions

---
+++ src/problems-by-topic/functions/merge.js
@@ -0,0 +1,20 @@
+import _ from 'lodash';
+
+export default (...objects) => objects.reduce((acc, obj) => {
+    const keys = Object.keys(obj);
+    for (const key of keys) {
+        if (!Object.hasOwn(acc, key)) {
+            acc[key] = [];
+          }
+        
+        acc[key].push(obj[key]);
+        acc[key] = _.uniq(acc[key]);
+
+    }
+
+    return acc;
+
+}, {});
+
+// const cons = (list, el) => _.union(list, [el]);
+// export default (...dictionaries) => _.mergeWith({}, ...dictionaries, cons);

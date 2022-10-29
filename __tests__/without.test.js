b75b412e922f485aec6ce61ab1c4141cfa657637
sulianova <ulianova.sofia@gmail.com>
2022-10-29 13:43:26 +0600

    Add option to without test

--- __tests__/without.test.js
+++ __tests__/without.test.js
@@ -6,4 +6,5 @@ test('without', () => {
   expect(without([2, 1, 2, 3], 1, 2)).toEqual([3]);
   expect(without([2, 1, 2, 3], 5)).toEqual([2, 1, 2, 3]);
   expect(without([], 5)).toEqual([]);
+  expect(without([3, 8, 9, 8, 10], 8, 10)).toEqual([3, 9]);
 });

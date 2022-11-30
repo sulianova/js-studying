779a76f0e0efbea64c542aebb5eac12eec329d54
sulianova <ulianova.sofia@gmail.com>
2022-11-30 17:11:55 +0600

    add getMutualFriends & rational in oop

---
+++ src/problems-by-topic/oop/getMutualFriends.js
@@ -0,0 +1,6 @@
+export default (user1, user2) => {
+  const friends1 = user1.getFriends();
+  const friends2 = user2.getFriends();
+  const friends2Ids = friends2.map(({ id }) => id);
+  return friends1.filter(({ id }) => friends2Ids.includes(id));
+};
---
+++ src/problems-by-topic/oop/rational.js
@@ -0,0 +1,26 @@
+const make = (numer, denom) => ({
+  numer,
+  denom,
+  setNumer(newNumer) {
+    this.numer = newNumer;
+  },
+  setDenom(newDenom) {
+    this.denom = newDenom;
+  },
+  getNumer() {
+    return this.numer;
+  },
+  getDenom() {
+    return this.denom;
+  },
+  toString() {
+    return `${this.getNumer()}/${this.getDenom()}`;
+  },
+  add(rational) {
+    const newNumer = this.getNumer() * rational.getDenom() + rational.getNumer() * this.getDenom();
+    const newDenom = this.getDenom() * rational.getDenom();
+    return make(newNumer, newDenom);
+  },
+});
+
+export default make;

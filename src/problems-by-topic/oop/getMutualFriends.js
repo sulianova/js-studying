449f29b692ea6063141d29c51df86b745653976c
sulianova <ulianova.sofia@gmail.com>
2022-12-03 18:30:50 +0600

    fix getMutualFriends in oop

--- src/problems-by-topic/oop/getMutualFriends.js
+++ src/problems-by-topic/oop/getMutualFriends.js
@@ -1,3 +1,11 @@
+export const user = ({ id = null, friends = [] } = {}) => ({
+  friends,
+  id,
+  getFriends() {
+    return this.friends.slice();
+  },
+});
+
 export default (user1, user2) => {
   const friends1 = user1.getFriends();
   const friends2 = user2.getFriends();

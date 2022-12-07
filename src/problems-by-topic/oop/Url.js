e3ba8ea67c58f367e6eeb5e80e8dcef13a6d5ecc
sulianova <ulianova.sofia@gmail.com>
2022-12-07 20:13:59 +0600

    add Url in oop

---
+++ src/problems-by-topic/oop/Url.js
@@ -0,0 +1,31 @@
+export default class Url {
+  constructor(url) {
+    this.url = new URL(url);
+    this.url.scheme = this.url.protocol.slice(0, -1);
+    this.url.queryParams = Object.fromEntries(this.url.searchParams);
+  }
+
+  getScheme() {
+    return this.url.scheme;
+  }
+
+  getHostName() {
+    return this.url.hostname;
+  }
+
+  getQueryParams() {
+    return this.url.queryParams;
+  }
+
+  getQueryParam(key, defaultValue = null) {
+    return this.url.searchParams.get(key) ?? defaultValue;
+  }
+
+  toString() {
+    return this.url.toString();
+  }
+
+  equals(url) {
+    return (this.toString() === url.toString());
+  }
+}

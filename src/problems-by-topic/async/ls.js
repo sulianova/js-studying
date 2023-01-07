396f8c88e9cafea0ddff9b640cc5040fad2deb73
sulianova <ulianova.sofia@gmail.com>
2023-01-07 22:14:47 +0600

    add ls in async

--- src/problems-by-topic/async/ls.js
+++ src/problems-by-topic/async/ls.js
@@ -2,32 +2,16 @@ import _ from 'lodash';
 import path from 'path';
 import fsp from 'fs/promises';
 
-// const dirPath = '/Users/sofiyaulianova/Desktop/web-developer/js-studying/src/problems-by-topic/async';
-const dirPath = '/Users/sofiyaulianova/Desktop/web-developer/js-studying/src/problems-by-topic/async/compareFileSizes.js';
-// export default (dirPath) => fsp.readdir(dirPath).then((filenames) => {
-// const f = (dirPath) => {
-//   if (fsp.stat.isFile(dirPath)) return [{ filepath: dirPath, mode }];
+export default async (pathForInspect) => {
+  const absolutePath = path.resolve(pathForInspect);
+  const stat = await fsp.stat(absolutePath);
+  if (stat.isFile()) {
+    return [{ filepath: absolutePath, mode: stat.mode }];
+  }
 
-//   fsp.stat.isFile(dirPath).then(isfile => 
-//     if (isFile)
-//   )
+  const filenames = await fsp.readdir(absolutePath);
+  const filepaths = filenames.sort().map((filename) => path.join(absolutePath, filename));
+  const stats = await Promise.all(filepaths.map((filePath) => fsp.stat(filePath)));
 
-const collectStatFromFile = (filePath) => fsp.stat
-
-const collectStatFromDir = (dirPath) => fsp.readdir(dirPath).then((filenames) => {
-  const filepaths = filenames.map((name) => path.join(dirPath, name));
-
-  const processPath = (filepath, result) => fsp
-    .stat(filepath)
-    .then((data) => [...result, { filepath, mode: data.mode }].sort(customSort))
-    .catch(() => [...result, null]);
-  
-  const resultPromise = filepaths.reduce(
-    (promise, filepath) => promise.then((result) => processPath(filepath, result)),
-    Promise.resolve([])
-  );
-
-  return resultPromise;
-});
-
-f(dirPath).then(res => console.log(res));
+  return _.zipWith(filepaths, stats, (filepath, { mode }) => ({ filepath, mode }));
+};
